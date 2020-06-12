b6f4c91cf6eb8ea5e86e1352295f37d762985bcd
Edward Aziz <edazizovv@gmail.com>
2020-06-12 20:35:28 +0300

    Feature: Time-Serial Cross-Validation: II

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ metadata = {'name': 'm_utils',
             'download_url': 'https://github.com/redjerdai/m_utils',
             'packages': setuptools.find_packages(),
             'include_package_data': True,
-            'version': '0.0.2.0',
+            'version': '0.0.2.1',
             'long_description': '',
             'python_requires': '>=3.6',
             'install_requires': ['mkl', 'numpy', 'scipy', 'matplotlib', 'pandas', 'scikit-learn']}# ['numpy==1.18.1', 'xlrd', 'openpyxl', 'pandas', 'mkl']}
